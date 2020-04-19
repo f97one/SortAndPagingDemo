025e4821a4d914508074eaf7568ff393f3d1fa1a
HAJIME Fukuna <f97one@gmail.com>
2020-04-19 04:29:13 +0000

    不要なインポートを消して再構成。

--- src/main/kotlin/nwt/formula97/webapps/sortandpagingdemo/web/MainController.kt
+++ src/main/kotlin/nwt/formula97/webapps/sortandpagingdemo/web/MainController.kt
@@ -11,9 +11,10 @@ import org.springframework.data.web.PageableDefault
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.util.StringUtils
-import org.springframework.web.bind.annotation.*
-import org.springframework.web.servlet.ModelAndView
-import javax.servlet.http.HttpSession
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class MainController {
